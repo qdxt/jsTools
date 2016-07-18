6165557bc1944d6ff45f8c01974d74edac96737c
xianrenqiu <xianrenqiu@7lk.com>
2016-07-18 15:02:15 +0800

    动一下

--- tools.js
+++ tools.js
@@ -1,7 +1,14 @@
 (function(){
+ 	var _ling = function(){};//声明对象
+ 		_ling.prototype = {
+	 		trim:function(str){
+				return str.replace(/(^\s*)|(\s*$)/g, "");
+			},
+			pad:function(num){
+				return num < 10 ? ('0'+num) : num;
+			}
 
-	var $ = function(){
-		return document.getElementById(id);
-	}
-	
+ 		};
+		_ling = window._ling = new _ling();  // 返回到window 全局变量下 以供其他人调用
+
 })();
