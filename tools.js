260ddb240d73b8496439aa0c316d922d15107235
yinwei <980165003@qq.com>
2017-04-19 16:04:37 +0800

    去掉基于jquery的方法

--- tools.js
+++ tools.js
@@ -13,24 +13,24 @@
 				//判断空字符串 空对象 空数组 和boolean
 				return !arg && arg!==0&&typeof arg!=="boolean"?true:false;
 			},
-			tips:function(text,seconds){
-				// tips 消息提示
-				$("<p class='tips'>").html(text)
-				$('body').append($("<p class='tips'>").html(text));
-				setTimeout("$('.tips').fadeOut()",seconds);
- 			},
+			// tips:function(text,seconds){
+			// 	// tips 消息提示
+			// 	$("<p class='tips'>").html(text)
+			// 	$('body').append($("<p class='tips'>").html(text));
+			// 	setTimeout("$('.tips').fadeOut()",seconds);
+ 			// },
  			alert:function(message){
  				//调用系统弹窗
  				window.alert(message);
- 			},
- 			tab:function(nav,target,ele,className){
- 				// tab切换 nav 为点击按钮 target 为要切换的div元素 ele 为事件 className 为切换样式
- 				$(nav).on(ele,function(){
- 					var index = $(this).index();
- 					$(this).addClass(className).siblings().removeClass(className);
- 					$(target).eq(index).show().siblings().hide();
- 				});
  			}
+ 			// tab:function(nav,target,ele,className){
+ 			// 	// tab切换 nav 为点击按钮 target 为要切换的div元素 ele 为事件 className 为切换样式
+ 			// 	$(nav).on(ele,function(){
+ 			// 		var index = $(this).index();
+ 			// 		$(this).addClass(className).siblings().removeClass(className);
+ 			// 		$(target).eq(index).show().siblings().hide();
+ 			// 	});
+ 			// }
 
 	};
 	_ling = window._ling = new _ling();
